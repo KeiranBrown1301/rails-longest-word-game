340a79fcf08a2f4ecf8876d92d92fe090ff4dc15
Keiran Brown <keiranbrown1301@gmail.com>
2022-01-06 21:27:04 +0900

    still struggling with the logic.

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,13 @@
+class GamesController < ApplicationController
+  def new
+    @letters = ('a'..'z').to_a.shuffle.first(10)
+  end
+
+  def score
+    if @letters.include?(params[:word])
+      "Cogratulations! #{params[:word]} is a valid English word!"
+    else
+      "Sorry but #{params[:word]} cannot be build out of #{@letters}"
+    end
+  end
+end
